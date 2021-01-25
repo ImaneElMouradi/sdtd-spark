7135dfebd23a8212210b052497a1c973a7232890
Amine Elmouradi <amineelmouradi@mazuki.local>
2021-01-25 19:43:21 +0100

    update script

--- startup_script.sh
+++ startup_script.sh
@@ -7,6 +7,6 @@
 # # ./start-slave.sh spark://127.0.0.1:7077, --host 127.0.0.1
 
 # BUILD SPARK APP AND SUBMIT IT
-sleep 1m
+sleep 2m &&
 cd $HOME/StreamHandler && sbt package && spark-submit --class StreamHandler --master "spark://172.31.48.37:7077,172.31.48.38:7077" --packages "org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1,com.datastax.spark:spark-cassandra-connector_2.12:3.0.0,com.datastax.cassandra:cassandra-driver-core:4.0.0" target/scala-2.12/stream-handler_2.12-1.0.jar > $HOME/spark.log
 
